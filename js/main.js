61e30e024c631425335b5b41c06e9c7a10b54c94
Carmen <ccincotti3@gmail.com>
2017-08-02 15:05:28 -0400

    finish splash, go onto eater

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-$("button").click(function(){
-    $( ".splash" ).replaceWith( "<h2>New heading</h2>" );
-    console.log("clcik");
+let indexButtons = $("button");
+
+// indexButtons.click(function(){
+//     $( ".splash" ).replaceWith( "<h2>New heading</h2>" );
+//     console.log("clcik");
+// });
+
+$(".index-buttons").one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
+   function(e) {
+     indexButtons.addClass("upAndDown")
+ });
+
+
+$("#hotdog").click(function() {
+  $(".splash").hide();
+  $(".eater-splash").show();
+});
+
+
+$("#headshot").click(function() {
+  alert( "Handler for headshot called." );
 });
