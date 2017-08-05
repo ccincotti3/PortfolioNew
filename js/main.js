d7c457e426fd0ee4e671683e01b515a5c126c551
Carmen <ccincotti3@gmail.com>
2017-08-05 16:20:12 -0400

    add new styling

--- js/main.js
+++ js/main.js
@@ -21,3 +21,35 @@ $("#hotdog").mouseenter(
   $("#hdvid").css("visibility", "hidden");
 }
 );
+
+$(window).scroll(handleHitTop);
+
+function handleHitTop(event) {
+    var currentScrollTopValue = $(this).scrollTop();
+
+    if (handleHitTop.lastTop === undefined) {
+        handleHitTop.lastTop = currentScrollTopValue ;
+
+        return;
+    }
+
+    if (handleHitTop.lastTop == 0 && currentScrollTopValue == 0) {
+        return;
+    }
+
+    handleHitTop.lastTop = currentScrollTopValue;
+    var eaterBar = $("#eater-bar")
+    if (handleHitTop.lastTop == 0) {
+      eaterBar.css("background-color","transparent");
+      console.log("to clear")
+        }
+
+    if (handleHitTop.lastTop != 0 && eaterBar.css("background-color") != "rgb(255, 255, 255)") {
+      eaterBar.css("background-color","white");
+      console.log("to white")
+    }
+}
+
+$(window).on('beforeunload', function() {
+   $(window).scrollTop(0);
+});
